6da83942626e804c71c82114af174fde6c3661ed
Christian Arndt <arndt@metratec.com>
2018-04-12 17:35:05 +0200

    Added positioning based on averaging over RSSI values

--- src/ros_ips/positioning.py
+++ src/ros_ips/positioning.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 """
-DESCRIPTION
+Use this class to perform indoor positioning with the metraTec IPS system. The position of a receiver is evaluated by
+the received messages of RF beacons placed in the environment. Requires a YAML file with the configuration of zones.
+
+Usage:
+Ininitalize the class by passing the directory of the config file. Then collect any number of beacon pings and use the
+get_zone() function to return the current zone. Passing a list of pings instead of a single ping leads to averaging
+over all the passed messages.
 """
 
 from zone import Zone
@@ -9,12 +15,22 @@ import yaml
 
 
 class Positioning:
+    """Use the metraTec IPS system to perform positioning of a receiver depending on placement of RF beacons."""
     def __init__(self, config_dir):
+        """
+        Initialize class object by passing the directory to a YAML config file containing zone information.
+        :param config_dir: String: directory of the YAML config file containing the definitions of zones
+        """
         # get data from config file and initialize zone objects
         self.zones = []
         self.parse_config(config_dir)
 
     def parse_config(self, yml_dir):
+        """
+        Read the YAML config files of zones, parse the data and create zone/beacon objects. The created zone objects
+        are appended to the classes list of zones for later use.
+        :param yml_dir: String: directory of the YAML config file containing the definitions of zones
+        """
         # open YAML config file and store content as dictionary
         with open(yml_dir, 'r') as stream:
             yml = yaml.load(stream)
@@ -34,11 +50,56 @@ class Positioning:
             self.zones.append(Zone(zone['name'], zone['frame_id'], zone['polygon'], beacons))
 
     def get_zone(self, pings):
-        """DESCRIPTION Get zone from a list of pings"""
-        # TODO check if messages are correct -> BCN -> SPACE -> 16 characters -> SPACE -> 4 characters
-        # TODO average over RSSI values
-        # TODO pick beacon with highest average
-        # TODO look for beacon in zones and return respective zone info
+        """
+        Take average RSSI value for each beacon ping contained in the passed list of beacon pings. Then return the
+        zone which the beacon with the highest value belongs to.
+        :param pings: List of Strings: beacon pings collected over time to average [BCN <EID> <RSSI>, ...]
+        :return: Zone/None: zone object of beacons with highest signal strength
+        """
+        # get means of RSSI values for each individual beacon
+        means = self.get_mean(pings)
+        # sort mean dictionary by values and look for corresponding beacon/zone
+        # if the top beacon is not found, continue with the next one until all beacons are exhausted
+        for key, value in sorted(means.items(), key=lambda (k, v): v, reverse=True):
+            # look for key (EID) in every zone and return when match is found
+            for z in self.zones:  # iterate over zones
+                for b in z.beacons:  # iterate over beacons inside the zone
+                    if b.eid == key:  # if the EID of the beacon is the same as the current key (EID) return the zone
+                        return z
+        # return None if no matching beacon has been found
+        print('None of the received beacon pings match a beacon from the configuration file. Returning \'None\'')
+        return None
+
+    @staticmethod
+    def get_mean(pings):
+        """
+        Compute the mean of RSSI values of a list of collected beacon pings.
+        Sort the passed list of pings by their unique EID and allocate their measured RSSI values. Then compute and
+        return the mean RSSI value of all received messages for each individual beacon.
+        :param pings: List of Strings: beacon pings collected over time to average [BCN <EID> <RSSI>, ...]
+        :return: Dict: key-value-pairs of EID and computed mean of RSSI values {<EID>: <AVERAGE_RSSI>, ...}
+        """
+        # create empty dictionary for <EID>: [<RSSI_VALUE>, ...] pairs
+        ordered_values = dict()
+        # loop through every entry in the list of received pings
+        for p in pings:
+            # split string on every SPACE
+            split = p.split(' ')
+            # check whether values are valid and skip current entry if they are not
+            if len(split) != 3 or split[0] != 'BCN' or len(split[1]) != 16 or len(split[2]) != 4:
+                print('Encountered invalid beacon ping: {}'.format(p))
+                continue
+            # append current RSSI value to respective entry in list if the current EID is already there
+            if split[1] in ordered_values:
+                ordered_values[split[1]].append(int(split[2]))
+            # create new entry in for EID-RSSI-pairs if the current EID is not yet in the dictionary
+            else:
+                ordered_values[split[1]] = [int(split[2])]
+        # compute average RSSI value for each beacon
+        for o in ordered_values:
+            ordered_values[o] = sum(ordered_values[o]) / len(ordered_values[o])
+        # return mean RSSI values attached to their respective EIDs
+        return ordered_values
 
 
 if __name__ == '__main__':
@@ -47,5 +108,5 @@ if __name__ == '__main__':
     dummy_pings = ['BCN 0123456789ABCDEF -060', 'BCN 0123456789ABCDEF -070', 'BCN 0123456789ABCDFF -070',
                    'BCN 0123456789ABCDFF -090', 'BCN 0123456789ABCFFF -070', 'BCN 0123456789ABCFFF -050',
                    'BCN 0123456789ABFFFF -070', 'BCN 0123456789ABFFFF -060', 'BCN 0123456789ABFFFF -090']
-    # TODO get zone
-    a = 0
+    zone = p.get_zone(dummy_pings)
+    breakpoint = 0
